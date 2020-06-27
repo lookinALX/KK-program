2d89f6abdc9317807cde77384914b1d66ec84fe1
lookinALX <67375219+lookinALX@users.noreply.github.com>
2020-06-27 20:58:43 +0300

    Update GraphicInterface.py

--- GraphicInterface.py
+++ GraphicInterface.py
@@ -103,38 +103,42 @@ class PageStation(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
-        fake_label = tk.Label(self)
-        fake_label.grid(row=6, column=1)
-
         label1 = tk.Label(self, text='Compressor:', font='Helvetica 11')
-        label1.grid(row=0, column=1, pady=5)
+        label1.grid(row=0, column=1, pady=3)
 
         label2 = tk.Label(self, text='Dryer:', font='Helvetica 11')
-        label2.grid(row=2, column=1, pady=5)
+        label2.grid(row=2, column=1, pady=3)
 
         label3 = tk.Label(self, text='Filter, DSH:', font='Helvetica 11')
-        label3.grid(row=4, column=1, pady=5)
+        label3.grid(row=4, column=1, pady=3)
 
         but_back_sp = ttk.Button(self, text='Start Page', command=lambda: controller.show_frame(StartPage))
-        but_back_sp.grid(row=7, column=0, pady=20, padx=20)
+        but_back_sp.grid(row=7, column=4, pady=20, padx=20)
 
-        but_add1 = ttk.Button(self, text='Add')
+        but_add1 = ttk.Button(self, text='Add', command=lambda: textbox.insert(0, equip_box1.get()))
         but_add1.grid(row=1, column=0, padx=20)
 
-        equip_box1 = ttk.Combobox(self, values=conf.Equipment.compressors)
+        equip_box1 = AutocompleteCombobox(self, values=conf.Equipment.compressors)
         equip_box1.grid(row=1, column=1, columnspan=2)
 
         but_add2 = ttk.Button(self, text='Add')
         but_add2.grid(row=3, column=0, padx=20)
 
-        equip_box2 = ttk.Combobox(self, values=conf.Equipment.dr_fl_dhs)
+        equip_box2 = AutocompleteCombobox(self, values=conf.Equipment.dr_fl_dhs)
         equip_box2.grid(row=3, column=1, columnspan=2)
 
         but_add3 = ttk.Button(self, text='Add')
-        but_add3.grid(row=5, column=0, padx=20)
+        but_add3.grid(row=5, column=0, padx=20, pady=5)
 
-        equip_box3 = ttk.Combobox(self, values=conf.Equipment.dr_fl_dhs)
+        equip_box3 = AutocompleteCombobox(self, values=conf.Equipment.dr_fl_dhs)
         equip_box3.grid(row=5, column=1, columnspan=2)
+
+        textbox = tk.Listbox(self, width=15, height=8)    # For test reason
+        textbox.grid(row=1, column=3, rowspan=4, padx=30)
+
+        scroll = tk.Scrollbar(self, command=textbox.yview)
+        scroll.grid(row=1, column=3, rowspan=4, sticky='nse')
+        textbox.config(yscrollcommand=scroll.set)
 
 
 class PageTwo(tk.Frame):
@@ -163,6 +167,57 @@ class ChildWindow(tk.Toplevel):
 
         self.grab_set()
         self.focus_set()
+
+
+class AutocompleteCombobox(ttk.Combobox):
+    def __init__(self, *args, **kwargs):
+        """Use our completion list as our drop down selection menu, arrows move through menu."""
+        super().__init__(*args, **kwargs)
+        self._completion_list = self['values']
+        self._hits = []
+        self._hit_index = 0
+        self.position = 0
+        self.bind('<KeyRelease>', self.handle_keyrelease)
+
+    def autocomplete(self, delta=0):
+        """autocomplete the Combobox, delta may be 0/1/-1 to cycle through possible hits"""
+        if delta:  # need to delete selection otherwise we would fix the current position
+            self.delete(self.position, tk.END)
+        else:  # set position to end so selection starts where text entry ended
+            self.position = len(self.get())
+        # collect hits
+        _hits = []
+        for element in self._completion_list:
+            if element.lower().startswith(self.get().lower()):  # Match case insensitively
+                _hits.append(element)
+        # if we have a new hit list, keep this in mind
+        if _hits != self._hits:
+            self._hit_index = 0
+            self._hits = _hits
+        # only allow cycling if we are in a known hit list
+        if _hits == self._hits and self._hits:
+            self._hit_index = (self._hit_index + delta) % len(self._hits)
+        # now finally perform the auto completion
+        if self._hits:
+            self.delete(0, tk.END)
+            self.insert(0, self._hits[self._hit_index])
+            self.select_range(self.position, tk.END)
+
+    def handle_keyrelease(self, event):
+        """event handler for the keyrelease event on this widget"""
+        if event.keysym == "BackSpace":
+            self.delete(self.index(tk.INSERT), tk.END)
+            self.position = self.index(tk.END)
+        if event.keysym == "Left":
+            if self.position < self.index(tk.END):  # delete the selection
+                self.delete(self.position, tk.END)
+            else:
+                self.position = self.position - 1  # delete one character
+                self.delete(self.position, tk.END)
+        if event.keysym == "Right":
+            self.position = self.index(tk.END)  # go to end (no selection)
+        if len(event.keysym) == 1:
+            self.autocomplete()
 
 
 app = WizardLikeApp()
