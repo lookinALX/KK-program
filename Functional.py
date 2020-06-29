b63891a42777bacf9f662465895b5ca53eb8565f
lookinALX <67375219+lookinALX@users.noreply.github.com>
2020-06-29 18:33:22 +0300

    Update Functional.py

--- Functional.py
+++ Functional.py
@@ -7,35 +7,32 @@ def csv_file_directory(directory):
     return df
 
 
-def New_Excel_creation(df, eq_name):
+def New_Excel_creation(df, equip):
     writer = pd.ExcelWriter('E:/Python Projects/KAESER_Program/new_file.xlsx')
-    select_rows(df, eq_name).to_excel(writer, 'Sheet1')
+    select_rows(df, equip).to_excel(writer, 'Sheet1')
     writer.save()
 
 
-def select_rows(df, eq_name, amount=1, change1='Wight', change2='Dimensions',
+def select_rows(df, equip, change1='Wight', change2='Dimensions',
                 eq='Equipment', pr='Price', fad='FAD'):
     """"Function selects particular rows and creates new DataFrame
         Takes DataFrame, change1 and change 2 (changeable name), eq='Equipment', pr='Price', fad='FAD'
-        Takes amount (normally input)
+        Takes equipment name list (normally input)
     """
     # Создание нового фрейма, хз так себе...
     new_df = pd.DataFrame({eq: '_', pr: '_', change1: '_', change2: '_', fad: '_'}, index=[0])
-    for i in range(amount):
+    for i in range(len(equip)):
         # Input and checks input
-        eq_name = check_input(eq_name, list(df['Equipment']))
+        if check_input(equip[i], list(df['Equipment'])):
+            return False
         # Search and receive data from DataFrame
-        search = df['Equipment'] == eq_name
+        search = df['Equipment'] == equip[i]
         new_df = pd.concat([new_df, df.loc[search]])
     return new_df
 
 
 def check_input(example, list_of_sth):
     """"Function checks the written category"""
-    i = True
-    while i:
-        if example in list_of_sth:
-            i = False
-        else:
-            example = input('There is not this category or equipment. Please enter correct one: ')
-    return example
+    if example not in list_of_sth:
+        return True
+
